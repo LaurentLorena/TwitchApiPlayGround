fb3529433933bc98318ddc592e8331e0c0317cea
LaurentLorena <laurentlorena@Laurents-MacBook-Pro.local>
2018-04-08 19:04:49 -0300

    Initial Commit

---
+++ TwitchApi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitchApi
+//
+//  Created by Laurent Lorena on 08/04/18.
+//  Copyright © 2018 Laurent Lorena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
